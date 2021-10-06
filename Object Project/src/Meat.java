fd3381056f2ecb937184813e40c12bd800c0b200
carterhill <carterhill@localhost>
2021-10-05 21:06:39 -0600

    home machine commit 10/5/21

--- Object Project/src/Meat.java
+++ Object Project/src/Meat.java
@@ -41,6 +41,7 @@ public class Meat
 					System.out.println("Press enter to shoot Dokkaebi.");
 		
 					String shoot1 = tanner1.nextLine();
+					
 				 int shot = (int) (Math.random() * 6) + 1;
 				 
 				 if(shot == 1 || shot == 2)
@@ -49,11 +50,12 @@ public class Meat
 						 System.out.println("Leg shot! 30 damage.");
 						 if(Operators.listOfOperators.get(playerIndexP1).getHealth() > 0)
 							 {
-						 System.out.println("Dokkaebi still has " + Operators.listOfOperators.get(playerIndexP1).getHealth() + " health left.");
+						 System.out.println("Dokkaebi has " + Operators.listOfOperators.get(playerIndexP1).getHealth() + " health left.");
 							 }
 						 if(Operators.listOfOperators.get(playerIndexP1).getHealth() <= 0)
 							 {
 								 System.out.println("Dokkaebi is dead.");
+								 System.out.println("Kill confirmed.");
 								 System.exit(shot);
 							 }
 					 }
@@ -63,29 +65,27 @@ public class Meat
 						 System.out.println("Chest shot! 55 damage.");
 						 if(Operators.listOfOperators.get(playerIndexP1).getHealth() > 0)
 							 {
-								 System.out.println("Dokkaebi still has " + Operators.listOfOperators.get(playerIndexP1).getHealth() + " health left.");
+								 System.out.println("Dokkaebi has " + Operators.listOfOperators.get(playerIndexP1).getHealth() + " health left.");
 							 }
 						 if(Operators.listOfOperators.get(playerIndexP1).getHealth() <= 0)
 							 {
 								 System.out.println("Dokkaebi is dead.");
+								 System.out.println("Kill confirmed.");
 								 System.exit(shot);
 							 }
 					 }
-				 		if(shot == 5 && shot == 6)
+				 		if(shot == 5 || shot == 6)
 				 			{
 				 				Operators.listOfOperators.get(playerIndexP1).setHealth(Operators.listOfOperators.get(playerIndexP1).getHealth() - 125);
 				 				System.out.println("Head shot!");
 				 				System.out.println("Dokkaebi is dead");
 				 				System.out.println("Kill confirmed.");
 				 				System.exit(shot);
+				 				
 				 			}
 				
 				 
-				 	if(Operators.listOfOperators.get(playerIndexP1).getHealth() <= 0 && shot != 6)
-				 		{
-				 			System.out.println("Kill confirmed.");
-				 			 System.exit(shot);
-				 		}
+				 
 					}			
 	
 		public static void Dokkaebi()
@@ -94,7 +94,8 @@ public class Meat
 						System.out.println("Press enter to shoot Warden.");
 					
 						String shoot2 = tanner2.nextLine();
-					 int shot = (int) (Math.random() * 6) + 1;
+						
+					 int shot = (int)(Math.random() * 6) + 1;
 					 
 					 if(shot == 1 || shot == 2 || shot == 3)
 						 {
@@ -102,11 +103,12 @@ public class Meat
 							 System.out.println("Leg shot! 30 damage.");
 							 if(Operators.listOfOperators.get(playerIndexP2).getHealth() > 0)
 								 {
-							 System.out.println("Warden still has " + Operators.listOfOperators.get(playerIndexP2).getHealth() + " health left.");
+							 System.out.println("Warden has " + Operators.listOfOperators.get(playerIndexP2).getHealth() + " health left.");
 								 }
 							 if(Operators.listOfOperators.get(playerIndexP2).getHealth() <= 0)
 								 {
 									 System.out.println("Warden is dead.");
+									 System.out.println("Kill confirmed.");
 									 System.exit(shot);
 								 }
 						 }
@@ -116,11 +118,12 @@ public class Meat
 							 System.out.println("Chest shot! 55 damage.");
 							 if(Operators.listOfOperators.get(playerIndexP2).getHealth() > 0)
 								 {
-							 System.out.println("Warden still has " + Operators.listOfOperators.get(playerIndexP2).getHealth() + " health left.");
+							 System.out.println("Warden has " + Operators.listOfOperators.get(playerIndexP2).getHealth() + " health left.");
 								 }
 							 if(Operators.listOfOperators.get(playerIndexP2).getHealth() <= 0)
 								 {
 									 System.out.println("Warden is dead.");
+									 System.out.println("Kill confirmed.");
 									 System.exit(shot);
 								 }
 						 }
@@ -134,10 +137,6 @@ public class Meat
 						 }
 					
 					 
-					 	if(Operators.listOfOperators.get(playerIndexP2).getHealth() <= 0 && shot != 6)
-					 		{
-					 			System.out.println("Kill confirmed.");
-					 			 System.exit(shot);
-					 		}	
+					 
 			}
 	}
